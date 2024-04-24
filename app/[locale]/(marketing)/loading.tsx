85700fc4f9ca54fff7b56ce52c41038397aadca0
Raphael Lima <oraphadev@gmail.com>
2024-04-23 23:46:32 -0300

    style(master): 🎨 loading page opacity

--- app/[locale]/(marketing)/loading.tsx
+++ app/[locale]/(marketing)/loading.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Loading = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 top-0 z-50 flex items-center justify-center bg-background">
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-50 flex items-center justify-center bg-background/30 backdrop-blur dark:bg-background/30">
       <div role="status">
         <Image
           src="/logo.svg"
